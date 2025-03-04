e7e5b418d3d5350a5fc3c6766f940b8967ef0715
Bart Wolleswinkel <b.wolleswinkel@tudelft.nl>
2025-03-04 10:17:26 +0100

    Updated documentation conf file with new author, version and project name information

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'GeoPES'
+copyright = '2025, Bart Wolleswinkel'
+author = 'Bart Wolleswinkel'
 
 release = '0.1'
-version = '0.1.0'
+version = '0.1.2'
 
 # -- General configuration
 
