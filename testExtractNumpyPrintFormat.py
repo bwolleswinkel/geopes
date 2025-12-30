d8298fbb56e94030ee56771cb4a2e87d12ad2708
Bart Wolleswinkel <b.wolleswinkel@tudelft.nl>
2025-12-30 20:37:12 +0100

    Also added method for ellipsoid, which also seems to work

--- testExtractNumpyPrintFormat.py
+++ testExtractNumpyPrintFormat.py
@@ -1,5 +1,6 @@
 """Script to test the extraction of NumPy print format settings."""
 
+import re
 from typing import Literal
 import warnings
 
@@ -8,14 +9,17 @@ import numpy as np
 # ------ PARAMETERS ------
 
 # Set the options for the integer array
-int_rows, int_cols = 5, 5
+int_rows, int_cols = 3, 3
 
 # Set the options for the floating array
 float_rows, float_cols = 3, 100
 
+# Set the options for ellipsoid
+n = 6
+
 # ------ METHOD ------
 
-def poly_hrepr(A: np.ndarray, b: np.ndarray, A_eq: np.ndarray | None = None, B_eq: np.ndarray | None = None, object_type: Literal['polyhedron', 'polytope', 'zonotope', 'cube'] = 'polytope', include_tabs: bool = True, trunc_mode: Literal['single_ellipsis', 'both_ellipsis'] = 'both_ellipsis', strip_double_b_brackets: bool = True) -> None:
+def poly_hrepr(A: np.ndarray, b: np.ndarray, A_eq: np.ndarray | None = None, B_eq: np.ndarray | None = None, object_type: Literal['polyhedron', 'polytope', 'zonotope', 'cube'] = 'polytope', include_tabs: bool = True, trunc_mode: Literal['single_ellipsis', 'both_ellipsis'] = 'both_ellipsis', strip_double_b_brackets: bool = True) -> str:
     #: Extract the dimensionality
     n = A.shape[1]
     #: Extract the Numpy print format with the right printing options
@@ -79,7 +83,7 @@ def poly_hrepr(A: np.ndarray, b: np.ndarray, A_eq: np.ndarray | None = None, B_e
     # Return the final string
     return str_header + '\n' + final_str
 
-def poly_vrepr(verts: np.ndarray, rays: np.ndarray | None = None, object_type: Literal['polyhedron', 'polytope', 'zonotope', 'cube'] = 'polytope', include_tabs: bool = True, trunc_mode: Literal['single_ellipsis', 'all_ellipsis'] = 'both_ellipsis', strip_double_brackets: bool = True, max_nverts: int = 5) -> None:
+def poly_vrepr(verts: np.ndarray, rays: np.ndarray | None = None, object_type: Literal['polyhedron', 'polytope', 'zonotope', 'cube'] = 'polytope', include_tabs: bool = True, trunc_mode: Literal['single_ellipsis', 'all_ellipsis'] = 'both_ellipsis', strip_double_brackets: bool = True, max_nverts: int = 5) -> str:
     #: Check the parameters
     if max_nverts < 3:
         raise NotImplementedError("max_nverts must be at least 3 to allow for truncation.")
@@ -166,6 +170,62 @@ def poly_vrepr(verts: np.ndarray, rays: np.ndarray | None = None, object_type: L
     # Return the final string
     return str_header + '\n' + final_str
 
+def ellipsoid_semidef_repr(c: np.ndarray, Q: np.ndarray, include_tabs: bool = True, object_type: Literal['polyhedron', 'polytope', 'zonotope', 'cube'] = 'polytope', filter_sym_part: bool = True) -> str:
+    #: Extract the dimensionality
+    n = Q.shape[1]
+    #: Extract the Numpy print format with the right printing options
+    # TODO: Move this to a config file?
+    # FIXME: If the line width is more then the array fits on one line, the truncation does not work as expected: it will insert line breaks, which will mess up the formatting
+    #: Extract the print options
+    edgeitems = np.get_printoptions()['edgeitems']
+    #: Extract the Numpy print format with the right printing options
+    with np.printoptions(edgeitems=edgeitems, threshold=edgeitems ** 2, linewidth=1000):
+        #: Flatten array is single row
+        if n == 1:
+            Q_str_lines, c_str_lines = [Q.__str__().replace('[', '').replace(']', '')], [c.__str__().replace('[', '').replace(']', '')]
+        else:
+            Q, c = Q, np.atleast_2d(c).T
+            Q_np_str, c_np_str = Q.__str__().replace(']', '] ').replace('] ] ', ']]'), c.__str__().replace(']', '] ').replace('] ] ', ']]')
+            Q_str_lines, c_str_lines = Q_np_str.splitlines(), c_np_str.splitlines()
+            Q_str_lines = [elem if elem != ' ...' else elem + (' ' * (len(Q_str_lines[-1]) - 4)) for elem in Q_str_lines]
+            c_str_lines = [elem if elem != ' ...' else elem + (' ' * (len(c_str_lines[-1]) - 4)) for elem in c_str_lines]
+    #: Retrieve the shape
+    n_rows = len(Q_str_lines)
+    #: Replace the symmetric part with `*` symbol and the appropriate number of spaces
+    if filter_sym_part:
+        for row_idx in range(n_rows):
+            row_elems = Q_str_lines[row_idx].split()
+            for col_idx in range(len(row_elems)):
+                if col_idx > row_idx:
+                    if '...' in row_elems[col_idx]:
+                        continue
+                    # FIXME: I very often get buggy behavior of `re` when running this script twice/multiple times in succession: "AttributeError: 'NoneType' object has no attribute 'start'"
+                    n_digits, start_idx = len(row_elems[col_idx].replace('[', '').replace(']', '')), re.search(r'\d', row_elems[col_idx]).start()
+                    # TODO: Make it such that this aligns nicely in the center of the decimal places
+                    Q_str_lines[row_idx] = Q_str_lines[row_idx].replace(row_elems[col_idx][start_idx:(start_idx + n_digits)], (' ' * (n_digits // 2)) + '*' + (' ' * ((n_digits // 2) if n_digits % 2 == 1 else (n_digits // 2 - 1))))
+    #: Create the 'tab' string  # NOTE: A \t is way too wide
+    tab_str_lines = ['  ' for _ in range(n_rows)] if include_tabs else ['' for _ in range(n_rows)]
+    #: Create the beginning c: string
+    center_offset_bottom = 0 if n_rows <= 2 else 1
+    begin_str_lines = (['   '] * (n_rows - center_offset_bottom - 1)) + ['c: '] + (['   '] * center_offset_bottom)
+    #: Create the intermediate comma string
+    comma_str_lines = (['  '] * (n_rows - center_offset_bottom - 1)) + [', '] + (['  '] * center_offset_bottom)
+    #: Create the beginning Q: string
+    begin_Q_str_lines = (['   '] * (n_rows - center_offset_bottom - 1)) + ['Q: '] + (['   '] * center_offset_bottom)
+    #: Create the ending return string
+    return_str_lines = ['\n' for _ in range(n_rows)]
+    #: Construct the final string line by line
+    final_str = ''
+    for row_idx in range(n_rows):
+        final_str += tab_str_lines[row_idx] + begin_str_lines[row_idx] + c_str_lines[row_idx] + comma_str_lines[row_idx] + begin_Q_str_lines[row_idx] + Q_str_lines[row_idx] + return_str_lines[row_idx]
+    #: Create the header string
+    match object_type:
+        case 'ellipsoid':
+            str_header = f"Ellipsoid in R^{n}"
+        case 'sphere':
+            raise NotImplementedError("Printing for sphere is not yet implemented.")
+    return str_header + '\n' + final_str
+
 # ------ SCRIPT ------
 
 # Create an integer array
@@ -173,16 +233,26 @@ def poly_vrepr(verts: np.ndarray, rays: np.ndarray | None = None, object_type: L
 int_array_A, int_array_b = np.random.rand(int_rows, int_cols), np.random.rand(int_rows) * 1E-6
 
 # Create a floating-point array
-float_array_verts = np.random.rand(float_rows, float_cols)
+float_array_verts = np.random.rand(float_rows, float_cols) * 1E-6
+
+# Create a positive semidefinite matrix for the ellipsoid
+# c, Q_root = np.random.randint(1000, 2000, size=n), np.random.randint(1000, 2000, size=(n, n))
+c, Q_root = np.random.rand(n), np.random.rand(n, n) * 1E-0
+Q = Q_root.T @ Q_root  # Make sure Q is positive semidefinite
 
 # ------ PRINT ------
 
-# Print the integer array
+# Print the polytope H-representation
 print("H-representation:")
-np.set_printoptions(suppress=False, precision=2, edgeitems=3)
+np.set_printoptions(suppress=False, precision=2, edgeitems=1)
 print(poly_hrepr(int_array_A, int_array_b, object_type='polytope', include_tabs=True, trunc_mode='single_ellipsis', strip_double_b_brackets=False))  # I think I prefer single_ellipsis here, and not stripping double brackets
 
-# Print the floating-point array
+# Print the polytope V-representation
 print("V-representation:")
-np.set_printoptions(suppress=True, precision=1, edgeitems=3)
-print(poly_vrepr(float_array_verts, object_type='polytope', include_tabs=True, trunc_mode='all_ellipsis', strip_double_brackets=False, max_nverts=4))
+np.set_printoptions(suppress=False, precision=1, edgeitems=3)
+print(poly_vrepr(float_array_verts, object_type='polytope', include_tabs=True, trunc_mode='all_ellipsis', strip_double_brackets=False, max_nverts=5))
+
+# Print the ellipsoid semidefinite representation
+print("Ellipsoid semidefinite representation:")
+np.set_printoptions(suppress=False, precision=3, edgeitems=2)
+print(ellipsoid_semidef_repr(c, Q, include_tabs=True, filter_sym_part=True, object_type='ellipsoid'))
