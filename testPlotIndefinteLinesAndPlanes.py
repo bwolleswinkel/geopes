b0b221ce0bd97747ef4657be5a0a129108f87f3c
Bart Wolleswinkel <b.wolleswinkel@tudelft.nl>
2026-01-11 00:22:23 +0100

    Also added planes, works flawlessly! Very nice!

--- testPlotIndefinteLinesAndPlanes.py
+++ testPlotIndefinteLinesAndPlanes.py
@@ -5,154 +5,186 @@ from numpy.typing import ArrayLike
 import matplotlib.pyplot as plt
 from matplotlib.axes import Axes
 
-def plot_line_2d(ax: Axes, line: ArrayLike, point: ArrayLike = np.array([0, 0]), **kwargs) -> None:
-    """Plot an indefinite line in 2D, defined by a point and a direction vector"""
+def plot_line(ax, line: ArrayLike, point: ArrayLike = None, **kwargs) -> None:
+    """Plot an indefinite line in 2D or 3D, defined by a point and a direction vector"""
 
     def update_line():
-        xlim, ylim = ax.get_xlim(), ax.get_ylim()
-        dx, dy = vx, vy
+        # Get axis limits
+        limits = [ax.get_xlim(), ax.get_ylim()] + ([ax.get_zlim()] if ndim == 3 else [])
+        
+        # Calculate t-values for intersections
         t_values = []
-        if abs(dx) > 1e-10:
-            t_left = (xlim[0] - center_x) / dx
-            t_right = (xlim[1] - center_x) / dx
-            t_values.extend([t_left, t_right])
-        if abs(dy) > 1e-10:
-            t_bottom = (ylim[0] - center_y) / dy
-            t_top = (ylim[1] - center_y) / dy
-            t_values.extend([t_bottom, t_top])
-        if abs(dx) < 1e-10:
-            x_coords = [center_x, center_x]
-            y_coords = [ylim[0] - abs(ylim[1] - ylim[0]), ylim[1] + abs(ylim[1] - ylim[0])]
-        elif abs(dy) < 1e-10:
-            x_coords = [xlim[0] - abs(xlim[1] - xlim[0]), xlim[1] + abs(xlim[1] - xlim[0])]
-            y_coords = [center_y, center_y]
+        coords = []
+        
+        for i, (lim, d, c) in enumerate(zip(limits, line, center)):
+            if abs(d) > 1e-10:
+                t_left, t_right = (lim[0] - c) / d, (lim[1] - c) / d
+                t_values.extend([t_left, t_right])
+            else:
+                # Line parallel to this axis - check if within bounds
+                if c < lim[0] or c > lim[1]:
+                    # Hide line if outside bounds in any parallel direction
+                    coords = [[] for _ in range(ndim)]
+                    break
+        
+        if coords == [] and t_values:  # Line is visible
+            if ndim == 2:
+                # 2D case with extension
+                t_min, t_max = min(t_values), max(t_values)
+                t_range = t_max - t_min
+                t_min -= t_range * 0.1
+                t_max += t_range * 0.1
+                coords = [center + t * line for t in [t_min, t_max]]
+                line_obj.set_xdata([c[0] for c in coords])
+                line_obj.set_ydata([c[1] for c in coords])
+            else:
+                # 3D case with clipping
+                dir_checks = [(limits[i], line[i], center[i]) for i in range(3)]
+                t_bounds = []
+                
+                for lim, d, c in dir_checks:
+                    if abs(d) > 1e-10:
+                        t1, t2 = (lim[0] - c) / d, (lim[1] - c) / d
+                        t_bounds.append([min(t1, t2), max(t1, t2)])
+                    else:
+                        t_bounds.append([-1e10, 1e10])
+                
+                t_enter = max(tb[0] for tb in t_bounds)
+                t_exit = min(tb[1] for tb in t_bounds)
+                
+                if t_enter <= t_exit:
+                    coords = [center + t * line for t in [t_enter, t_exit]]
+                    line_obj.set_data_3d([c[0] for c in coords], [c[1] for c in coords], [c[2] for c in coords])
+                else:
+                    line_obj.set_data_3d([], [], [])
         else:
-            t_min, t_max = min(t_values), max(t_values); t_range = t_max - t_min
-            t_min -= t_range * 0.1
-            t_max += t_range * 0.1
-            x_coords, y_coords = [center_x + t_min * dx, center_x + t_max * dx], [center_y + t_min * dy, center_y + t_max * dy]
-        line.set_xdata(x_coords), line.set_ydata(y_coords)
+            # Handle parallel case or empty coords
+            if ndim == 2 and not coords:
+                # 2D parallel line case
+                range_max = max(abs(lim[1] - lim[0]) for lim in limits)
+                coords = [center + t * line for t in [-range_max, range_max]]
+                line_obj.set_xdata([c[0] for c in coords])
+                line_obj.set_ydata([c[1] for c in coords])
+            elif ndim == 3:
+                line_obj.set_data_3d([], [], [])
     
-    point = np.array(point, dtype=float)
     line = np.array(line, dtype=float)
     if np.allclose(line, 0):
         raise ValueError("Vector cannot be zero")
     
-    # Calculate the center point and direction from the span vector
-    x0, y0 = point
-    vx, vy = line
-    center_x = x0 + vx / 2
-    center_y = y0 + vy / 2
+    ndim = len(line)
     
-    line, = ax.plot([], [], **kwargs)
-    ax.callbacks.connect('xlim_changed', lambda ax: update_line())
-    ax.callbacks.connect('ylim_changed', lambda ax: update_line())
+    if point is None:
+        point = np.zeros(ndim)
+    point = np.array(point, dtype=float)
+    if len(point) != ndim:
+        raise ValueError(f"Point must be {ndim}D for {ndim}D line")
+    
+    # Calculate center point
+    center = point + line / 2
+    
+    # Create line object and setup callbacks
+    if ndim == 1:
+        raise NotImplementedError("1D lines are not supported yet")
+    elif ndim == 2:
+        line_obj, = ax.plot([], [], **kwargs)
+        callbacks = ['xlim_changed', 'ylim_changed']
+    elif ndim == 3:
+        line_obj, = ax.plot([], [], [], **kwargs)
+        callbacks = ['xlim_changed', 'ylim_changed', 'zlim_changed']
+    else:
+        raise ValueError("Line vector must be 2D or 3D")
+    
+    for callback in callbacks:
+        ax.callbacks.connect(callback, lambda _: update_line())
+    
     update_line()
 
 
-def plot_line_3d(ax, line: ArrayLike, point: ArrayLike = np.array([0, 0, 0]), **kwargs) -> None:
-    """Plot an indefinite line in 3D, defined by a point and a direction vector"""
-
-    def update_line():
+def plot_plane(ax: Axes, plane_normal: ArrayLike, point: ArrayLike = None, **kwargs) -> None:
+    """Plot an indefinite plane in 3D, defined by a normal vector and a point"""
+    from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+    
+    def update_plane():
         xlim, ylim, zlim = ax.get_xlim(), ax.get_ylim(), ax.get_zlim()
-        dx, dy, dz = vx, vy, vz
-        t_values = []
         
-        if abs(dx) > 1e-10:
-            t_left = (xlim[0] - center_x) / dx
-            t_right = (xlim[1] - center_x) / dx
-            t_values.extend([t_left, t_right])
-        if abs(dy) > 1e-10:
-            t_bottom = (ylim[0] - center_y) / dy
-            t_top = (ylim[1] - center_y) / dy
-            t_values.extend([t_bottom, t_top])
-        if abs(dz) > 1e-10:
-            t_near = (zlim[0] - center_z) / dz
-            t_far = (zlim[1] - center_z) / dz
-            t_values.extend([t_near, t_far])
+        # Find intersections with cube edges
+        intersections = []
+        edges = [
+            ([xlim[0], xlim[1]], ylim[0], zlim[0]), ([xlim[0], xlim[1]], ylim[1], zlim[0]),
+            ([xlim[0], xlim[1]], ylim[0], zlim[1]), ([xlim[0], xlim[1]], ylim[1], zlim[1]),
+            (xlim[0], [ylim[0], ylim[1]], zlim[0]), (xlim[1], [ylim[0], ylim[1]], zlim[0]),
+            (xlim[0], [ylim[0], ylim[1]], zlim[1]), (xlim[1], [ylim[0], ylim[1]], zlim[1]),
+            (xlim[0], ylim[0], [zlim[0], zlim[1]]), (xlim[0], ylim[1], [zlim[0], zlim[1]]),
+            (xlim[1], ylim[0], [zlim[0], zlim[1]]), (xlim[1], ylim[1], [zlim[0], zlim[1]]),
+        ]
         
-        if len(t_values) > 0:
-            t_min, t_max = min(t_values), max(t_values)
-            
-            # Check if line actually intersects the 3D box
-            if abs(dx) > 1e-10:
-                x_min_t = (xlim[0] - center_x) / dx
-                x_max_t = (xlim[1] - center_x) / dx
-                if x_min_t > x_max_t:
-                    x_min_t, x_max_t = x_max_t, x_min_t
+        for edge in edges:
+            if isinstance(edge[0], list):
+                y_val, z_val = edge[1], edge[2]
+                if abs(normal[0]) > 1e-10:
+                    x_intersect = (d - normal[1]*y_val - normal[2]*z_val) / normal[0]
+                    if xlim[0] <= x_intersect <= xlim[1]:
+                        intersections.append([x_intersect, y_val, z_val])
+            elif isinstance(edge[1], list):
+                x_val, z_val = edge[0], edge[2]
+                if abs(normal[1]) > 1e-10:
+                    y_intersect = (d - normal[0]*x_val - normal[2]*z_val) / normal[1]
+                    if ylim[0] <= y_intersect <= ylim[1]:
+                        intersections.append([x_val, y_intersect, z_val])
             else:
-                # Line parallel to x-planes: check if within x bounds
-                if center_x < xlim[0] or center_x > xlim[1]:
-                    x_coords, y_coords, z_coords = [], [], []
-                    line_obj.set_data_3d(x_coords, y_coords, z_coords)
-                    return
-                x_min_t, x_max_t = -1e10, 1e10
-                
-            if abs(dy) > 1e-10:
-                y_min_t = (ylim[0] - center_y) / dy
-                y_max_t = (ylim[1] - center_y) / dy
-                if y_min_t > y_max_t:
-                    y_min_t, y_max_t = y_max_t, y_min_t
+                x_val, y_val = edge[0], edge[1]
+                if abs(normal[2]) > 1e-10:
+                    z_intersect = (d - normal[0]*x_val - normal[1]*y_val) / normal[2]
+                    if zlim[0] <= z_intersect <= zlim[1]:
+                        intersections.append([x_val, y_val, z_intersect])
+        
+        if intersections:
+            intersections = np.unique(np.array(intersections), axis=0)
+            if len(intersections) >= 3:
+                center = np.mean(intersections, axis=0)
+                u = intersections[1] - intersections[0]
+                v = np.cross(normal, u)
+                if np.linalg.norm(v) > 1e-10:
+                    v = v / np.linalg.norm(v)
+                    u = u / np.linalg.norm(u)
+                    coords_2d = [(np.dot(p - center, u), np.dot(p - center, v)) for p in intersections]
+                    angles = [np.arctan2(y, x) for x, y in coords_2d]
+                    ordered_points = intersections[np.argsort(angles)]
+                    poly_collection.set_verts([ordered_points])
+                else:
+                    poly_collection.set_verts([intersections])
             else:
-                # Line parallel to y-planes: check if within y bounds
-                if center_y < ylim[0] or center_y > ylim[1]:
-                    x_coords, y_coords, z_coords = [], [], []
-                    line_obj.set_data_3d(x_coords, y_coords, z_coords)
-                    return
-                y_min_t, y_max_t = -1e10, 1e10
-                
-            if abs(dz) > 1e-10:
-                z_min_t = (zlim[0] - center_z) / dz
-                z_max_t = (zlim[1] - center_z) / dz
-                if z_min_t > z_max_t:
-                    z_min_t, z_max_t = z_max_t, z_min_t
-            else:
-                # Line parallel to z-planes: check if within z bounds
-                if center_z < zlim[0] or center_z > zlim[1]:
-                    x_coords, y_coords, z_coords = [], [], []
-                    line_obj.set_data_3d(x_coords, y_coords, z_coords)
-                    return
-                z_min_t, z_max_t = -1e10, 1e10
-            
-            # Find intersection with 3D box
-            t_enter = max(x_min_t, y_min_t, z_min_t)
-            t_exit = min(x_max_t, y_max_t, z_max_t)
-            
-            # Only show line if it actually intersects the box
-            if t_enter <= t_exit:
-                x_coords = [center_x + t_enter * dx, center_x + t_exit * dx]
-                y_coords = [center_y + t_enter * dy, center_y + t_exit * dy]
-                z_coords = [center_z + t_enter * dz, center_z + t_exit * dz]
-            else:
-                # Line doesn't intersect current view - hide it
-                x_coords, y_coords, z_coords = [], [], []
+                poly_collection.set_verts([])
         else:
-            # Handle case where line is parallel to all coordinate planes
-            range_max = max(abs(xlim[1] - xlim[0]), abs(ylim[1] - ylim[0]), abs(zlim[1] - zlim[0]))
-            t_min, t_max = -range_max * 0.1, range_max * 0.1
-            x_coords = [center_x + t_min * dx, center_x + t_max * dx]
-            y_coords = [center_y + t_min * dy, center_y + t_max * dy]
-            z_coords = [center_z + t_min * dz, center_z + t_max * dz]
-        
-        line_obj.set_data_3d(x_coords, y_coords, z_coords)
+            poly_collection.set_verts([])
     
+    plane_normal = np.array(plane_normal, dtype=float)
+    if np.allclose(plane_normal, 0):
+        raise ValueError("Normal vector cannot be zero")
+    
+    if len(plane_normal) != 3:
+        raise ValueError("Plane normal must be 3D")
+    
+    if point is None:
+        point = np.zeros(3)
     point = np.array(point, dtype=float)
-    line = np.array(line, dtype=float)
-    if np.allclose(line, 0):
-        raise ValueError("Vector cannot be zero")
+    if len(point) != 3:
+        raise ValueError("Point must be 3D for 3D plane")
     
-    # Calculate the center point and direction from the span vector
-    x0, y0, z0 = point
-    vx, vy, vz = line
-    center_x = x0 + vx / 2
-    center_y = y0 + vy / 2
-    center_z = z0 + vz / 2
+    # Normalize normal vector and calculate plane constant
+    normal = plane_normal / np.linalg.norm(plane_normal)
+    d = np.dot(normal, point)
     
-    line_obj, = ax.plot([], [], [], **kwargs)
-    ax.callbacks.connect('xlim_changed', lambda _: update_line())
-    ax.callbacks.connect('ylim_changed', lambda _: update_line())
-    ax.callbacks.connect('zlim_changed', lambda _: update_line())
-    update_line()
+    # Create polygon collection
+    poly_collection = Poly3DCollection([], linewidths=0, **kwargs)
+    ax.add_collection3d(poly_collection)
+    
+    # Set up callbacks
+    for callback in ['xlim_changed', 'ylim_changed', 'zlim_changed']:
+        ax.callbacks.connect(callback, lambda _: update_plane())
+    
+    update_plane()
 
 
 if __name__ == "__main__":
@@ -160,20 +192,29 @@ if __name__ == "__main__":
     # ------ 2D LINE ------
     fig, ax = plt.subplots()
     
-    plot_line_2d(ax, line=[1, 1], color='red', linewidth=2)
-    plot_line_2d(ax, point=[1, 1], line=[-1, 2], color='blue')
+    plot_line(ax, line=[1, 1], color='red', linewidth=2)
+    plot_line(ax, point=[1, 1], line=[-1, 2], color='blue')
     ax.plot([0, 2, 1, 0], [0, 1, 1, 3], 'go', label='Points')
 
     # ------ 3D LINE ------
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
 
-    plot_line_3d(ax, line=[1, 1, 0], color='red', linewidth=2)
-    plot_line_3d(ax, line=[-1, 2, 1], color='blue')
+    plot_line(ax, line=[1, 1, 0], color='red', linewidth=2)
+    plot_line(ax, line=[-1, 2, 1], color='blue')
     ax.scatter([0, 2, 1, 0], [0, 1, 1, 3], [1, 0, 2, 3], c='g', label='Points', axlim_clip=True)
     ax.set_xlim(-3, 3)
     ax.set_ylim(-3, 3)
     ax.set_zlim(-3, 3)
+
+    # ------ 3D PLANE ------
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    plot_line(ax, line=[1, 2, 0], color='red', linewidth=2)
+    plot_plane(ax, plane_normal=np.array([1, 1, 1]), color='orange', alpha=0.5)
+    ax.set_xlim(-3, 3)
+    ax.set_ylim(-3, 3)
+    ax.set_zlim(-10, 10)
 
     # Show plots
     plt.show()
